58575907457db677d8a468968ca3eba90eb38b80
IzakDev <141975061+IzakStudios@users.noreply.github.com>
2024-02-09 12:40:22 +0000

    98hojhhiljhhoh0j

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
+function getMousePos(ctx, mouseEvent) {
+    var rect = ctx.getBoundingClientRect();
+
+    return {
+        x: (ctx.clientX - rect.left) * scale,
+        y: (ctx.clientY - rect.top) * scale,
+    };
+}
+
+class Camera {
+    constructor(){
+        this._position = [0, 0];
+    }
+
+    update(){ this._position[0] = this._position[0] + 1 };
+
+    setPosition(position){ this._position = position};
+    getPosition(){ return this._position };
+}
+
+const gameCamera = new Camera();
+
 function drawCircle(obj) {
+    const cameraPosition = gameCamera.getPosition();
+
+    obj.x = obj.x - cameraPosition[0]
+    obj.y = obj.y + cameraPosition[1]
+
     obj.ctx.beginPath();
     obj.ctx.arc(obj.x, obj.y, obj.radius, 0, 2 * Math.PI, false);
-
+    
     if (obj.fill) {
         obj.ctx.fillStyle = obj.fill;
 
@@ -16,123 +43,115 @@ function drawCircle(obj) {
     }
 }
 
-function getMousePos(ctx, mouseEvent) {
-    var rect = ctx.getBoundingClientRect();
+class Object {
+    constructor(position){
+        this.isRemoved = false;
 
-    return {
-        x: (ctx.clientX - rect.left) * scale,
-        y: (ctx.clientY - rect.top) * scale,
-    };
-}
-
-var pressedKeys = {};
-var playerLocation = [-200, 0];
-
-var playerJumping = false;
-var playerSpeed = 2;
-var playerGravity = 1;
-var playerColor = "rgb(0,255,255)";
-var playerVelocityIncrease = 2;
-var previousLocations = [];
-
-var lastTick = Date.now() / 1000;
-
-function updatePhysics(){
-    let playerVelocity = playerJumping && 1 || -1;
-
-    if (previousLocations.length >= 350){
-        previousLocations = previousLocations.slice(1, previousLocations.length)
-    };
-
-    switch (playerGravity){
-        case -1:
-            playerColor = "rgb(255,255,0)";
-            playerVelocityIncrease = 1;
-            break
-        case 1:
-            playerColor = "rgb(0,255,255)";
-            playerVelocityIncrease = 2;
-            break
-    };
-
-    previousLocations.push([playerLocation[1], playerColor]);
-
-    let nextLocation = playerLocation[1] + playerVelocity * playerSpeed * playerGravity * playerVelocityIncrease;
-    if (Math.abs(nextLocation) >= gameCanvas.height / 2){
-        playerGravity *= -1;
-        nextLocation = playerLocation[1] + playerVelocity * playerSpeed * playerGravity * playerVelocityIncrease;
+        this._position = position;
+        this._color = "#FF0000";
+        this._radius = 15;
     }
 
-    playerLocation[1] = Math.max(
-        -gameCanvas.height / 2,
-        Math.min(
-            gameCanvas.height / 2,
-            nextLocation
-        )
-    );
+    update(){
+        const cameraPosition = gameCamera.getPosition();
+        if (this._position[0] < cameraPosition[0] - gameCanvas.width / 2 - this._radius){
+            this.remove()
+        };
+    };
+    draw(canvasContext){
+        drawCircle({
+            ctx: canvasContext,
+            x: gameCanvas.width / 2 + this._position[0],
+            y: gameCanvas.height / 2 - this._position[1],
+            radius: this._radius,
+            fill: "#FF00FF",
+        })
+    }
+    remove(){ this.isRemoved = true };
 }
 
-function renderGame(){
-    var currentTick = Date.now() / 1000;
-    var deltaTime = currentTick - lastTick;
+class Player {
+    constructor(){
+        this._position = [0, 0];
+        this._gravity = 1;
+        this._mode = 1;
+        this._isJumping = false;
+        this.speed = 9;
+    }
 
-    lastTick = Date.now() / 1000;
+    setJumping(state){ this._isJumping = state };
 
-    updatePhysics();
+    setPosition(position){ this._position = position};
+    getPosition(){ return this._position };
 
-    var ctx = gameCanvas.getContext("2d");
-    ctx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
-    
-    var centerX = gameCanvas.width / 2;
-    var centerY = gameCanvas.height / 2;
+    async update(){
+        let x = 1;
+        let y = -1;
 
-    previousLocations.forEach((locationData, index) => {
+        if (this._isJumping){ y = 1; };
+
+        this._position[0] += x * this.speed;
+        this._position[1] += y * this.speed;
+    };
+
+    async draw(canvasContext){
         drawCircle({
-            ctx: ctx,
-            x: centerX + playerLocation[0] - (previousLocations.length - index) * playerSpeed,
-            y: centerY - locationData[0],
-            radius: 15,
-            fill: locationData[1],
+            ctx: canvasContext,
+            x: gameCanvas.width / 2 + this._position[0],
+            y: gameCanvas.height / 2 - this._position[1],
+            radius: 20,
+            fill: "#00FFFF",
         })
-    });
+    };
+}
 
-    drawCircle({
-        ctx: ctx,
-        x: centerX + playerLocation[0],
-        y: centerY - playerLocation[1],
-        radius: 20,
-        fill: playerColor,
-    });
+const lerp = (a, b, t) => {
+    return a + (b - a) * t;
+}
+
+var heldKeys = {};
+var gamePlayer = new Player();
+var objects = [];
+
+async function renderGame(){
+    const canvasContext = gameCanvas.getContext("2d");
+    canvasContext.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+    gamePlayer.setJumping(heldKeys["ArrowUp"] || heldKeys["Space"]);
+    gamePlayer.update();
+    gamePlayer.draw(canvasContext);
+
+    let playerPosition = gamePlayer.getPosition();
+    let cameraPosition = gameCamera.getPosition();
+
+    gameCamera.setPosition([
+        lerp(cameraPosition[0], playerPosition[0], 0.1),
+        lerp(cameraPosition[1], playerPosition[1], 0.02),
+    ]);
+
+    if (Math.floor(Math.random() * Math.floor(30 - gamePlayer.speed)) == 1){
+        objects.push(new Object([
+            playerPosition[0] + gameCanvas.width / 1.75,
+            playerPosition[1] + (Math.floor(Math.random() * gameCanvas.height) - gameCanvas.height / 2)
+        ]))
+    };
+
+    objects.forEach((object, index) => {
+        object.update();
+        object.draw(canvasContext);
+
+        if (object.isRemoved){ objects.splice(index, 1) };
+    })
+
+    requestAnimationFrame(renderGame);
 };
 
-window.addEventListener("keydown", (event) => {
-    if (pressedKeys[event.code]) return;
-    pressedKeys[event.code] = true;
-
-    let keyCode = event.key;
-    switch (keyCode){
-        case "ArrowUp":
-            playerJumping = true;
-            break;
-    };
-});
-window.addEventListener("keyup", (event) => {
-    pressedKeys[event.code] = false;
-
-    let keyCode = event.key;
-    switch (keyCode){
-        case "ArrowUp":
-            playerJumping = false;
-            break;
-    }
-});
+window.addEventListener("keydown", (event) => { heldKeys[event.code] = true; });
+window.addEventListener("keyup", (event) => { heldKeys[event.code] = false; });
 
 window.addEventListener("load", async () => {
     gameCanvas.width = 1024;
     gameCanvas.height = 1024;
 
-    while (true) {
-        requestAnimationFrame(renderGame)
-        await new Promise(r => setTimeout(r, 1 / 30));
-    };
+    requestAnimationFrame(renderGame)
 })
